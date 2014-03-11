73eda1f7e93630981a16a1d4bb54fd743d90ed47
NikolayDachev <nikolay@dachev.info>
2014-03-11 12:21:00 +0200

    jadm python setup -to be tested

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(
+    name = "jadm",
+    version = "0.7",
+    packages = find_packages(),
+    scripts = ['jadm'],
+
+    # install or upgraded on the target machine
+    # - tabulate - used to print useful infomration in table format
+    # - netifaces - used for check FreeBSD bridge interfaces
+    # - ipaddress - used to check if jail ip is from gateway network
+
+    install_requires = ['tabulate>=0.7', 'netifaces>=0.6', 'ipaddress>=1.0.6'],
+
+    # metadata for upload to PyPI
+    author = "Nikolay Georgiev Dachev",
+    author_email = "jadm@dachev.info",
+    description = "JADM is a easy FreeBSD 10 jail framework with /etc/jail.conf, vnet and zfs support ",
+    license = "BSD 3-Clause",
+    keywords = "jadm vnet zfs jail.conf",
+    url = "https://github.com/NikolayDachev/jadm",   # project home page, if any
+)
