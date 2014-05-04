abc0decbd93443af6913a0ea99995cc1ae7155ca
NikolayDachev <nikolay@dachev.info>
2014-05-04 23:05:01 +0300

    jadm ver. 8.5 migrate function added to be tested

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     # - netifaces - used for check FreeBSD bridge interfaces
     # - ipaddress - used to check if jail ip is from gateway network
 
-    install_requires = ['tabulate>=0.7', 'netifaces>=0.6', 'ipaddress>=1.0.6'],
+    install_requires = ['tabulate>=0.7', 'netifaces>=0.6', 'ipaddress>=1.0.6', 'paramiko>=1.13.0'],
 
     # metadata for upload to PyPI
     author = "Nikolay Georgiev Dachev",
