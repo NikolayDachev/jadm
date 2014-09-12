2b8f05bc9ec847dfb2b9008c154a0b50a2d00f18
NikolayDachev <nikolay@dachev.info>
2014-09-12 21:35:16 +0300

    ver 1.0 download py libs

---
+++ pylibs/tabulate-0.7.2/test/test_input.py
@@ -0,0 +1,336 @@
+# -*- coding: utf-8 -*-
+
+"""Test support of the various forms of tabular data."""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from tabulate import tabulate
+
+
+def test_iterable_of_iterables():
+    "Input: an interable of iterables."
+    ii = iter(map(lambda x: iter(x), [range(5), range(5,0,-1)]))
+    expected = "\n".join(
+        ['-  -  -  -  -',
+         '0  1  2  3  4',
+         '5  4  3  2  1',
+         '-  -  -  -  -'])
+    result   = tabulate(ii)
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
+
+
+def test_iterable_of_iterables_headers():
+    "Input: an interable of iterables with headers."
+    ii = iter(map(lambda x: iter(x), [range(5), range(5,0,-1)]))
+    expected = "\n".join(
+        ['  a    b    c    d    e',
+         '---  ---  ---  ---  ---',
+         '  0    1    2    3    4',
+         '  5    4    3    2    1'])
+    result   = tabulate(ii, "abcde")
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
+
+
+def test_iterable_of_iterables_firstrow():
+    "Input: an interable of iterables with the first row as headers"
+    ii = iter(map(lambda x: iter(x), ["abcde", range(5), range(5,0,-1)]))
+    expected = "\n".join(
+        ['  a    b    c    d    e',
+         '---  ---  ---  ---  ---',
+         '  0    1    2    3    4',
+         '  5    4    3    2    1'])
+    result   = tabulate(ii, "firstrow")
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
+
+
+def test_list_of_lists():
+    "Input: a list of lists with headers."
+    ll = [["a","one",1],["b","two",None]]
+    expected = "\n".join([
+        '    string      number',
+        '--  --------  --------',
+        'a   one              1',
+        'b   two'])
+    result   = tabulate(ll, headers=["string","number"])
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
+
+
+def test_list_of_lists_firstrow():
+    "Input: a list of lists with the first row as headers."
+    ll = [["string","number"],["a","one",1],["b","two",None]]
+    expected = "\n".join([
+        '    string      number',
+        '--  --------  --------',
+        'a   one              1',
+        'b   two'])
+    result   = tabulate(ll, headers="firstrow")
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
+
+
+def test_list_of_lists_keys():
+    "Input: a list of lists with column indices as headers."
+    ll = [["a","one",1],["b","two",None]]
+    expected = "\n".join([
+        '0    1      2',
+        '---  ---  ---',
+        'a    one    1',
+        'b    two'])
+    result   = tabulate(ll, headers="keys")
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
+
+
+def test_dict_like():
+    "Input: a dict of iterables with keys as headers."
+    # columns should be padded with None, keys should be used as headers
+    dd = {"a": range(3), "b": range(101,105)}
+    # keys' order (hence columns' order) is not deterministic in Python 3
+    # => we have to consider both possible results as valid
+    expected1 = "\n".join([
+        '  a    b',
+        '---  ---',
+        '  0  101',
+        '  1  102',
+        '  2  103',
+        '     104'])
+    expected2 = "\n".join([
+        '  b    a',
+        '---  ---',
+        '101    0',
+        '102    1',
+        '103    2',
+        '104'])
+    result    = tabulate(dd, "keys")
+    print("Keys' order: %s" % dd.keys())
+    print("Expected 1:\n%s\n" % expected1)
+    print("Expected 2:\n%s\n" % expected2)
+    print("Got:\n%s\n" % result)
+    assert result in [expected1, expected2]
+
+
+def test_numpy_2d():
+    "Input: a two-dimensional NumPy array with headers."
+    try:
+        import numpy
+        na = (numpy.arange(1,10, dtype=numpy.float32).reshape((3,3))**3)*0.5
+        expected = "\n".join([
+            '    a      b      c',
+            '-----  -----  -----',
+            '  0.5    4     13.5',
+            ' 32     62.5  108',
+            '171.5  256    364.5'])
+        result   = tabulate(na, ["a", "b", "c"])
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%s\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_numpy_2d is skipped")
+        pass   # this test is optional
+
+
+def test_numpy_2d_firstrow():
+    "Input: a two-dimensional NumPy array with the first row as headers."
+    try:
+        import numpy
+        na = (numpy.arange(1,10, dtype=numpy.int32).reshape((3,3))**3)
+        expected = "\n".join([
+            '  1    8    27',
+            '---  ---  ----',
+            ' 64  125   216',
+            '343  512   729'])
+        result   = tabulate(na, headers="firstrow")
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%r\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_numpy_2d_firstrow is skipped")
+        pass   # this test is optional
+
+
+def test_numpy_2d_keys():
+    "Input: a two-dimensional NumPy array with column indices as headers."
+    try:
+        import numpy
+        na = (numpy.arange(1,10, dtype=numpy.float32).reshape((3,3))**3)*0.5
+        expected = "\n".join([
+            '    0      1      2',
+            '-----  -----  -----',
+            '  0.5    4     13.5',
+            ' 32     62.5  108',
+            '171.5  256    364.5'])
+        result   = tabulate(na, headers="keys")
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%s\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_numpy_2d_keys is skipped")
+        pass   # this test is optional
+
+
+def test_numpy_record_array():
+    "Input: a two-dimensional NumPy record array without header."
+    try:
+        import numpy
+        na = numpy.asarray([("Alice", 23, 169.5),
+                            ("Bob", 27, 175.0)],
+                           dtype={"names":["name","age","height"],
+                                  "formats":["a32","uint8","f32"]})
+        expected = "\n".join([
+            "-----  --  -----",
+            "Alice  23  169.5",
+            "Bob    27  175",
+            "-----  --  -----" ])
+        result   = tabulate(na)
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%s\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_numpy_2d_keys is skipped")
+        pass   # this test is optional
+
+
+def test_numpy_record_array_keys():
+    "Input: a two-dimensional NumPy record array with column names as headers."
+    try:
+        import numpy
+        na = numpy.asarray([("Alice", 23, 169.5),
+                            ("Bob", 27, 175.0)],
+                           dtype={"names":["name","age","height"],
+                                  "formats":["a32","uint8","f32"]})
+        expected = "\n".join([
+            "name      age    height",
+            "------  -----  --------",
+            "Alice      23     169.5",
+            "Bob        27     175"  ])
+        result   = tabulate(na, headers="keys")
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%s\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_numpy_2d_keys is skipped")
+        pass   # this test is optional
+
+
+def test_numpy_record_array_headers():
+    "Input: a two-dimensional NumPy record array with user-supplied headers."
+    try:
+        import numpy
+        na = numpy.asarray([("Alice", 23, 169.5),
+                            ("Bob", 27, 175.0)],
+                           dtype={"names":["name","age","height"],
+                                  "formats":["a32","uint8","f32"]})
+        expected = "\n".join([
+            "person      years     cm",
+            "--------  -------  -----",
+            "Alice          23  169.5",
+            "Bob            27  175" ])
+        result   = tabulate(na, headers=["person", "years", "cm"])
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%s\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_numpy_2d_keys is skipped")
+        pass   # this test is optional
+
+
+def test_pandas():
+    "Input: a Pandas DataFrame."
+    try:
+        import pandas
+        df = pandas.DataFrame([["one",1],["two",None]], index=["a","b"])
+        expected = "\n".join([
+            '    string      number',
+            '--  --------  --------',
+            'a   one              1',
+            'b   two            nan'])
+        result   = tabulate(df, headers=["string", "number"])
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%s\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_pandas is skipped")
+        pass   # this test is optional
+
+
+def test_pandas_firstrow():
+    "Input: a Pandas DataFrame with the first row as headers."
+    try:
+        import pandas
+        df = pandas.DataFrame([["one",1],["two",None]],
+                              columns=["string","number"],
+                              index=["a","b"])
+        expected = "\n".join([
+            'a    one      1.0',
+            '---  -----  -----',
+            'b    two      nan'])
+        result   = tabulate(df, headers="firstrow")
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%s\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_pandas_firstrow is skipped")
+        pass   # this test is optional
+
+
+def test_pandas_keys():
+    "Input: a Pandas DataFrame with keys as headers."
+    try:
+        import pandas
+        df = pandas.DataFrame([["one",1],["two",None]],
+                              columns=["string","number"],
+                              index=["a","b"])
+        expected = "\n".join(
+            ['    string      number',
+             '--  --------  --------',
+             'a   one              1',
+             'b   two            nan'])
+        result   = tabulate(df, headers="keys")
+        print("Expected:\n%s\n" % expected)
+        print("Got:\n%s\n" % result)
+        assert expected == result
+    except ImportError:
+        print("test_pandas_keys is skipped")
+        pass   # this test is optional
+
+
+def test_list_of_namedtuples():
+    "Input: a list of named tuples with field names as headers."
+    from collections import namedtuple
+    NT = namedtuple("NT", ['foo', 'bar'])
+    lt = [NT(1,2), NT(3,4)]
+    expected = "\n".join([
+        '-  -',
+        '1  2',
+        '3  4',
+        '-  -'])
+    result = tabulate(lt)
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
+
+
+def test_list_of_namedtuples_keys():
+    "Input: a list of named tuples with field names as headers."
+    from collections import namedtuple
+    NT = namedtuple("NT", ['foo', 'bar'])
+    lt = [NT(1,2), NT(3,4)]
+    expected = "\n".join([
+        '  foo    bar',
+        '-----  -----',
+        '    1      2',
+        '    3      4'])
+    result = tabulate(lt, headers="keys")
+    print("Expected:\n%s\n" % expected)
+    print("Got:\n%s\n" % result)
+    assert expected == result
---
+++ pylibs/tabulate-0.7.2/test/test_regression.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+"""Regression tests."""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from tabulate import tabulate
+from common import assert_equal
+
+
+def test_ansi_color_in_table_cells():
+    "Regression: ANSI color in table cells (issue #5)."
+    colortable = [('test', '\x1b[31mtest\x1b[0m', '\x1b[32mtest\x1b[0m')]
+    colorlessheaders = ('test', 'test', 'test')
+    formatted = tabulate(colortable, colorlessheaders, 'pipe')
+    expected = "\n".join(['| test   | test   | test   |',
+                          '|:-------|:-------|:-------|',
+                          '| test   | \x1b[31mtest\x1b[0m   | \x1b[32mtest\x1b[0m   |'])
+    print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
+    assert_equal(expected, formatted)
+
+
+def test_alignment_of_colored_cells():
+    "Regression: Align ANSI-colored values as if they were colorless."
+    colortable = [('test', 42, '\x1b[31m42\x1b[0m'), ('test', 101, '\x1b[32m101\x1b[0m')]
+    colorheaders = ('test', '\x1b[34mtest\x1b[0m', 'test')
+    formatted = tabulate(colortable, colorheaders, 'grid')
+    expected = '\n'.join(['+--------+--------+--------+',
+                          '| test   |   \x1b[34mtest\x1b[0m |   test |',
+                          '+========+========+========+',
+                          '| test   |     42 |     \x1b[31m42\x1b[0m |',
+                          '+--------+--------+--------+',
+                          '| test   |    101 |    \x1b[32m101\x1b[0m |',
+                          '+--------+--------+--------+'])
+    print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
+    assert_equal(expected, formatted)
+
+
+def test_iter_of_iters_with_headers():
+    "Regression: Generator of generators with a generator of headers (issue #9)."
+
+    def mk_iter_of_iters():
+        def mk_iter():
+            for i in range(3):
+                yield i
+        for r in range(3):
+            yield mk_iter()
+
+    def mk_headers():
+        for h in ["a", "b", "c"]:
+            yield h
+
+    formatted = tabulate(mk_iter_of_iters(), headers=mk_headers())
+    expected = '\n'.join(['  a    b    c',
+                          '---  ---  ---',
+                          '  0    1    2',
+                          '  0    1    2',
+                          '  0    1    2'])
+    print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
+    assert_equal(expected, formatted)
+
+
+def test_datetime_values():
+    "Regression: datetime, date, and time values in cells (issue #10)."
+    import datetime
+    dt = datetime.datetime(1991,2,19,17,35,26)
+    d = datetime.date(1991,2,19)
+    t = datetime.time(17,35,26)
+    formatted = tabulate([[dt, d, t]])
+    expected = '\n'.join(['-------------------  ----------  --------',
+                          '1991-02-19 17:35:26  1991-02-19  17:35:26',
+                          '-------------------  ----------  --------'])
+    print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
+    assert_equal(expected, formatted)
+
+
+def test_simple_separated_format():
+    "Regression: simple_separated_format() accepts any separator (issue #12)"
+    from tabulate import simple_separated_format
+    fmt = simple_separated_format("!")
+    expected = 'spam!eggs'
+    formatted = tabulate([["spam", "eggs"]], tablefmt=fmt)
+    print("expected: %r\n\ngot:      %r\n" % (expected, formatted))
+    assert_equal(expected, formatted)
+
+
+def py3test_require_py3():
+    "Regression: py33 tests should actually use Python 3 (issue #13)"
+    from platform import python_version_tuple
+    print("Expected Python version: 3.x.x")
+    print("Python version used for tests: %s.%s.%s" % python_version_tuple())
+    assert_equal(python_version_tuple()[0], '3')
+
+
+def test_simple_separated_format_with_headers():
+    "Regression: simple_separated_format() on tables with headers (issue #15)"
+    from tabulate import simple_separated_format
+    expected = '  a|  b\n  1|  2'
+    formatted = tabulate([[1,2]], headers=["a", "b"], tablefmt=simple_separated_format("|"))
+    assert_equal(expected, formatted)
+
+
+def test_column_type_of_bytestring_columns():
+    "Regression: column type for columns of bytestrings (issue #16)"
+    from tabulate import _column_type, _binary_type
+    result = _column_type([b"foo", b"bar"])
+    expected = _binary_type
+    assert_equal(result, expected)
