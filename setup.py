15b24ae4f3b9178ff154ac644ce540f7b1f2fbd2
Charlie Root <root@jadm.dachev.info>
2014-09-13 00:57:04 +0300

    ver 1.0 - official release

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ def read(fname):
 setup(
     name = "jadm",
     version = "1.0",
-    packages = find_packages(),
+    package_dir = {'': 'lib'},
+    packages = ['ipaddress', 'netifaces', 'paramiko', 'tabulate'],
     scripts = ['jadm'],
 
     # install or upgraded on the target machine
     # - tabulate - used to print useful infomration in table format
     # - netifaces - used for check FreeBSD bridge interfaces
     # - ipaddress - used to check if jail ip is from gateway network
-
-    install_requires = ['tabulate>=0.7', 'netifaces>=0.6', 'ipaddress>=1.0.6', 'paramiko>=1.13.0'],
+    # install_requires = ['tabulate>=0.7', 'netifaces>=0.6', 'ipaddress>=1.0.6', 'paramiko>=1.13.0'],
 
     # metadata for upload to PyPI
     author = "Nikolay Georgiev Dachev",
