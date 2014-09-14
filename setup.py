0b394407d9f0ed358661c07c8a3c6e09d74c64d7
NikolayDachev <nikolay@dachev.info>
2014-09-14 12:06:03 +0300

    ver 1.0 - official release

--- setup.py
+++ setup.py
@@ -21,6 +21,10 @@ setup(
 
     install_requires = ['tabulate>=0.7', 'netifaces>=0.6', 'ipaddress>=1.0.6', 'paramiko>=1.13.0'],
 
+    packages=['man8'],
+    package_dir={'man8': 'man8'},
+    package_data={'man8': ['*.8']},
+
     # metadata for upload to PyPI
     author = "Nikolay Georgiev Dachev",
     author_email = "jadm@dachev.info",
